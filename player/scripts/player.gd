344d4f47057dad06ddc02cea3ee415666424bb73
michaelmalaska <97186701+michaelmalaska@users.noreply.github.com>
2025-10-16 20:23:12 -0600

    Ch. 01 Ep. 06 Crouch State & Jump Through/One-way Platforms

--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -3,6 +3,13 @@ class_name Player extends CharacterBody2D
 const DEBUG_JUMP_INDICATOR = preload("uid://1n5lkptfbcul")
 
 
+#region /// on ready variables
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var collision_stand: CollisionShape2D = $CollisionStand
+@onready var collision_crouch: CollisionShape2D = $CollisionCrouch
+@onready var one_way_platform_raycast: RayCast2D = $OneWayPlatformRaycast
+#endregion
+
 #region /// export variables
 @export var move_speed : float = 150
 #endregion
