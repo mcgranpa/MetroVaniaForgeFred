90e07fbaf006c01918cc45a4927d7250b2bfd07b
michaelmalaska <97186701+michaelmalaska@users.noreply.github.com>
2025-12-08 23:05:54 -0700

    Ep. 09-10 - Particles, Fog, Player Spawner, Etc

---
+++ general/player_spawn/player_spawn.gd
@@ -0,0 +1,21 @@
+@icon( "res://general/icons/player_spawn.svg" )
+class_name PlayerSpawn extends Node2D
+
+
+
+func _ready() -> void:
+	visible = false
+	await get_tree().process_frame
+	
+	if get_tree().get_first_node_in_group( "Player" ):
+		# We have a player!
+		print("We have a player")
+		return
+	
+	print( "No player found" )
+	# Instantiate a new instance of our player scene
+	var player : Player = load("uid://cfa3xni5hj64u").instantiate()
+	get_tree().root.add_child( player )
+	# Position the player scene
+	player.global_position = self.global_position
+	pass
